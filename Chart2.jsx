25f21ce875ee078b9528b38f8dc27bdfe95fb688
Mattsi <mattsi.jansky@codurance.com>
2020-02-08 10:19:28 +0000

    Compute highest/lowest values of data

--- Chart2.jsx
+++ Chart2.jsx
@@ -25,29 +25,38 @@ class MyChart extends PureComponent {
       indexRight: '',
       bottom: 'auto',
       top: 'auto',
+      data: this.addHighestAndLowestFields(props.data)
     }
   }
 
-  zoom() {
-    let { refAreaLeft, refAreaRight, indexLeft, indexRight } = this.state
-    
-    this.setState(() => ({
-      refAreaLeft: '',
-      refAreaRight: '',
-      left: refAreaLeft,
-      right: refAreaRight,
-      bottom: 'auto',
-      top: 'auto'
-    }))
+  addHighestAndLowestFields(data) {
+    data.forEach(datum => {
+      let highestQuantity = 1
+      let lowestQuantity = 9999
+
+      for(var key in datum) {
+        if(key != "date") {
+          const value = datum[key]
+          if(value > highestQuantity) highestQuantity = value
+          if(value < lowestQuantity) lowestQuantity = value
+        }
+      }
+
+      datum.highestQuantity = highestQuantity
+      datum.lowestQuantity = lowestQuantity
+    })
+
+    return data
   }
 
   render() {
-    const { left, right, refAreaLeft, refAreaRight, top, bottom } = this.state
-    console.log(`refAreaLeft: ${refAreaLeft}, refAreaRight: ${refAreaRight}, indexLeft: ${this.state.indexLeft}, indexRight: ${this.state.indexRight}`)
+    const { left, right, refAreaLeft, refAreaRight, top, bottom, data } = this.state
+    console.log(`data: ${JSON.stringify(data)}`)
+    //console.log(`refAreaLeft: ${refAreaLeft}, refAreaRight: ${refAreaRight}, indexLeft: ${this.state.indexLeft}, indexRight: ${this.state.indexRight}`)
 
     return <ResponsiveContainer width="99%" height={320}>
       <LineChart 
-        data={this.props.data}
+        data={data}
         onMouseDown={e => this.setState({ refAreaLeft: e.activeLabel, indexLeft: e.activeTooltipIndex })}
         onMouseMove={e => this.state.refAreaLeft && this.setState({ refAreaRight: e.activeLabel, indexRight: e.activeTooltipIndex })}
         onMouseUp={this.zoom.bind(this)}
